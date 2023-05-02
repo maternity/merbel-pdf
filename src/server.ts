b6e8ac754ffac93473b6e29bfd4d0de4addaf3f1
Kai Groner <kai.g@quiltedhealth.com>
2023-05-02 11:43:57 -0500

    Add concurrency knob.

--- src/server.ts
+++ src/server.ts
@@ -15,31 +15,36 @@ const developmentLoggingConfig: LoggerOptions = {
     },
   };
 
-const fastify = Fastify({
-    logger: process.env.NODE_ENV === 'production'
-      ? productionLoggingConfig
-      : developmentLoggingConfig,
-  });
-fastify.register(metrics);
-fastify.register(pdfService);
-
-async function serve(port=0) {
-  fastify.listen({host: '::', port})
-  await once(process, 'SIGTERM');
-  await fastify.close();
-}
-
 if (require.main === module) {
   const parser = new ArgumentParser();
 
   parser.add_argument('port', {metavar: 'PORT', nargs: '?', type: 'int', default: 80,
     help: 'Port to serve merbel-pdf on'});
+  parser.add_argument('--concurrency', '-j', {metavar: 'N', type: 'int',
+    help: 'Number of concurrent PDF conversions to process (additional '+
+      'requests will queue until running requests have completed)'});
 
-  const {port} = parser.parse_args();
+  const {port, concurrency} = parser.parse_args();
+
+  const fastify = Fastify({
+      logger: process.env.NODE_ENV === 'production'
+	? productionLoggingConfig
+	: developmentLoggingConfig,
+    });
+
+  fastify.register(metrics);
+  fastify.register(pdfService, {concurrency});
 
   serve(port)
     .catch((err) => {
       console.error(err);
       process.exit(1);
     });
+
+  async function serve(port=0) {
+    fastify.listen({host: '::', port})
+    await once(process, 'SIGTERM');
+    await fastify.close();
+  }
+
 }
